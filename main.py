64ea0a91f17e4f529dfba88c7fa0ad11b3f2e4f4
Kamran Feroz <kamranferoz@yahoo.com>
2023-09-06 14:51:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -134,17 +134,17 @@ def main():
         # Display word details in a table
         display_word_details(pdf_text, most_used=most_used, num_words=200)
         
-        # Write linkedin and other credentials on the sidebar footer
-        # Include sidebar with credentials
-        with st.sidebar:
-            # st.markdown('Chat With DIDX.net (V 0.1)')
-            st.markdown("""
-                        #### PDF to Word Cloud Generator (V 1.5) 
-                        #### Let's connect: [Kamran Feroz](https://www.linkedin.com/in/kamranferoz/)
-                        """)
-        st.markdown(
-            "<style>#MainMenu{visibility:hidden;}</style>",
-            unsafe_allow_html=True)        
+    # Write linkedin and other credentials on the sidebar footer
+    # Include sidebar with credentials
+    with st.sidebar:
+        # st.markdown('Chat With DIDX.net (V 0.1)')
+        st.markdown("""
+                    #### PDF to Word Cloud Generator (V 1.5) 
+                    #### Let's connect: [Kamran Feroz](https://www.linkedin.com/in/kamranferoz/)
+                    """)
+    st.markdown(
+        "<style>#MainMenu{visibility:hidden;}</style>",
+        unsafe_allow_html=True)        
         
 if __name__ == "__main__":
     main()
